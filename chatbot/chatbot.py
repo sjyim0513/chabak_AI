0d197bd50cf4f9fe695559efaffd61c9c9c6b5ec
sjyim0513 <sjyim0513@naver.com>
2023-06-01 19:43:23 +0900

    123

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -2,7 +2,6 @@ import os
 import openai
 import spacy
 import requests
-from dotenv import load_dotenv
 from konlpy.tag import Komoran
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -10,7 +9,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-load_dotenv()
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 class OpenAIGpt:
